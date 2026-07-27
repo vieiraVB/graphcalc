6c97580a1ec7b55bc34321a7f05fb79a3d90beba
Vitor Vieira <vitor.vieirab34@gmail.com>
2026-07-27 18:17:08 -0300

    refactor: organizacao da estrutura

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,6 @@
 const primeiroGrau = require("./calculadora/primeiroGrau");
 const interpretador = require("./interpretador/interpretarFuncao");
 
-
-console.log("GraphCalc iniciado");
-
 module.exports = {
     primeiroGrau,
     interpretador
