7f533f1c531e2ce2841f02993ad8941f2e682ab1
Vitor Vieira <vitor.vieirab34@gmail.com>
2026-07-27 18:07:47 -0300

    refactor: prganizar estrutura de modulos e testes

--- tests/primeiroGrau.test.js
+++ tests/primeiroGrau.test.js
@@ -4,6 +4,4 @@ console.log(primeiroGrau.calcularFuncaoPrimeiroGrau(2,5));
 console.log(primeiroGrau.calcularPonto(2,5,10));
 console.log(primeiroGrau.calcularRaiz(2,5));
 console.log(primeiroGrau.gerarPontos(2,5,-2,2));
-console.log(
-    primeiroGrau.analisarFuncaoPrimeiroGrau(2,5)
-);
+console.log(primeiroGrau.analisarFuncaoPrimeiroGrau(2,5));
