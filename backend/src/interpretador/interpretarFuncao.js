6fa751b8b74d8b1af6b588162d62fe4960df778c
Vitor Vieira <vitor.vieirab34@gmail.com>
2026-07-27 17:21:47 -0300

    feat: implementar interpretador de funcoes do primeiro grau

--- backend/src/interpretador/interpretarFuncao.js
+++ backend/src/interpretador/interpretarFuncao.js
@@ -1,27 +1,42 @@
 function interpretarFuncao(expressao) {
     expressao = expressao.replaceAll(" ", "");
+    expressao = expressao.toLowerCase();
 
     let posicaoX = expressao.indexOf("x");
 
     let erro = "nao eh uma expressao valida";
 
+    if (posicaoX === -1) {
+        return erro;
+    }
+
     let a = expressao.substring(0, posicaoX);
     let b = expressao.substring(posicaoX + 1);
+    let textoB = expressao.substring(posicaoX + 1);
     b = Number(b);
     a = Number(a);
 
-    if(expressao[0] === "x"){
+    if (expressao[0] === "x") {
         a = 1;
-    } else if (Number.isNaN(a)){
-        if(expressao[0] === "-"){
+    } else if (Number.isNaN(a)) {
+        if (expressao[0] === "-") {
             a = -1;
         } else {
             return erro;
         }
     }
 
-    return {a, b};
+    if (textoB === "") {
+        b = 0;
+    } else {
+        b = Number(textoB);
 
+        if (Number.isNaN(b)) {
+            return erro;
+        }
+    }
+
+    return { a, b };
 }
 
 module.exports = {
--- tests/testInterpretador.js
+++ tests/testInterpretador.js
@@ -1,3 +1,18 @@
-const interpretador = require("../backend/src/interpretador/interpretarFuncao");
+const { interpretarFuncao } = require("../backend/src/interpretador/interpretarFuncao");
 
-console.log(interpretador.interpretarFuncao("abcx"));
+console.log(interpretarFuncao("x"));
+
+console.log(interpretarFuncao("2x+5"));
+
+console.log(interpretarFuncao("2x - 5"));
+
+console.log(interpretarFuncao("-x+8"));
+
+console.log(interpretarFuncao("x-3"));
+
+console.log(interpretarFuncao("2x"));
+
+console.log(interpretarFuncao("abc"));
+
+console.log(interpretarFuncao("2x+abc"));
+
